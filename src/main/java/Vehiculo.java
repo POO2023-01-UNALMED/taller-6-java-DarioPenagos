79e4bd887d535d68b31c48c42ea19d86b6e98583
DarioPenagos <dpenagosv@unal.edu.co>
2023-03-26 12:37:28 -0500

    Update Vehiculo.java

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -102,7 +102,7 @@ public class Vehiculo{
 
     
     public static Fabricante fabricaMayorVentas(){
-        Fabricante m = new Fabricante("noo", new Pais("nononononononoo"));
+        Fabricante m;
         int n = 0;
         for(Map.Entry<Fabricante,Integer> entry : fab.entrySet()){
             if (n == 0 || entry.getValue() > n){
