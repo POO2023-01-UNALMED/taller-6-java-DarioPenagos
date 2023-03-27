db918384035b6f925dec814256fe507edc6c3dbb
DarioPenagos <dpenagosv@unal.edu.co>
2023-03-26 23:20:22 -0500

    Update Vehiculo.java

--- src/main/java/Vehiculo.java
+++ src/main/java/Vehiculo.java
@@ -1,122 +1,105 @@
 package vehiculos;
 
-import java.util.ArrayList;
+public class Vehiculo {
+	private String placa;
+	private int puertas;
+	private int velocidadMaxima;
+	private String nombre;
+	private int precio;
+	private int peso;
+	private String traccion;
+	private Fabricante fabricante;
+	private static int cantidadVehiculos;
+	
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
 
-public class Vehiculo{
-    String placa;
-    int puertas;
-    int velocidadMaxima;
-    String nombre;
-    int precio;
-    int peso;
-    String traccion;
-    Fabricante fabricante;
-    static int CantidadVehiculos = 0;
-    static int cantidadAutos = 0;
-    static int cantidadCamionetas = 0;
-    static int cantidadCamiones =0;
-    //static ArrayList<Integer> numFab = new ArrayList<Integer>();
-    //static ArrayList<Fabricante> fabr = new ArrayList<Fabricante>();
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		
+		if (Pais.ventasPorPais.containsKey(fabricante.getPais())) {
+			 Pais.ventasPorPais.put(fabricante.getPais(), Pais.ventasPorPais.get(fabricante.getPais()) + 1); 
+	    } else {
+	    	 Pais.ventasPorPais.put(fabricante.getPais(), 1);    
+	    }
+		
+		if (Fabricante.ventasPorFabrica.containsKey(fabricante)) {
+			Fabricante.ventasPorFabrica.put(fabricante, Fabricante.ventasPorFabrica.get(fabricante) + 1);
+	    } else {
+	    	Fabricante.ventasPorFabrica.put(fabricante, 1);
+	    }
+		
+		cantidadVehiculos++;	
+	}
 
-    public Vehiculo(String placa, int puertas, int vmax, String nom, int precio, int peso, String trac, Fabricante f){
-        this.placa = placa;
-        this.puertas = puertas;
-        this.velocidadMaxima = vmax;
-        this.nombre = nom;
-        this.precio = precio;
-        this.peso = peso;
-        this.traccion = trac;
-        this.fabricante = f;
-        if (trac.equals("FWD")){
-            this.cantidadAutos++;
-        } else if (trac.equals("4X2")){
-            this.cantidadCamiones++;
-        } else if (trac.equals("4X4")){
-            this.cantidadCamionetas++;
-        }
-        /*
-        if(!fabr.contains(f)){
-            fabr.add(f);
-            numFab.add(1);
-        }
-        */
-        //System.out.println(fabr);
-    }
+	public static int getCantidadVehiculos() {
+		return cantidadVehiculos;
+	}
 
-    public static String vehiculosPorTipo(){
-        return "Automoviles: " + cantidadAutos + "Camionetas: " + cantidadCamionetas + "Camiones: " + cantidadCamiones;
-    }
-    public static int getCantidadVehiculos(){
-        return Vehiculo.CantidadVehiculos;
-    }
-    public static void setCantidadVehiculos(int c){
-        Vehiculo.CantidadVehiculos = c;
-    }
+	public static void setCantidadVehiculos(int cantidadVehiculos) {
+		Vehiculo.cantidadVehiculos = cantidadVehiculos;
+	}
+	
+	public static String vehiculosPorTipo()
+	{
+		return "Automoviles: " + Automovil.getCantidadAutomoviles()+
+				"\nCamionetas: " + Camioneta.getCantidadCamionetas() +
+				"\nCamiones: " + Camion.getCantidadCamiones();
+	}
+	
+	public String getPlaca() {
+		return placa;
+	}
+	
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public int getPuertas() {
+		return puertas;
+	}
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
+	}
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPrecio() {
+		return precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public String getTraccion() {
+		return traccion;
+	}
+	public void setTraccion(String traccion) {
+		this.traccion = traccion;
+	}
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
 
-    public String getPlaca(){
-        return this.placa;
-    }
-    public void setPlaca(String p){
-        this.placa = p;
-    }
-    public int getPuertas(){
-        return this.puertas;
-    }
-    public void setPuertas(int p){
-        this.puertas = p;
-    }
-    public int getVelocidadMaxima(){
-        return this.velocidadMaxima;
-    }
-    public void setVelocidadMaxima(int v){
-        this.velocidadMaxima = v;
-    }
-    public String getNombre(){
-        return this.nombre;
-    }
-    public void setNombre(String n){
-        this.placa = n;
-    }
-    public int getPrecio(){
-        return this.precio;
-    }
-    public void setPrecio(int p){
-        this.precio = p;
-    }
-    public int getPeso(){
-        return this.peso;
-    }
-    public void setPeso(int p){
-        this.peso = p;
-    }
-    public String getTraccion(){
-        return this.traccion;
-    }
-    public void setTraccion(String t){
-        this.traccion = t;
-    }
-    public Fabricante getFabricante(){
-        return this.fabricante;
-    }
-    public void setFabricante(Fabricante f){
-        this.fabricante = f;
-    }
-
-    /*
-    public static Fabricante fabricaMayorVentas(){
-        Fabricante m = null;
-        int n = 0;
-        for(Map.Entry<Fabricante,Integer> entry : fab.entrySet()){
-            if (n == 0 || entry.getValue() > n){
-                m = entry.getKey();
-                n = entry.getValue();
-            }
-        }
-        if (m == null){
-            return new Fabricante("Renault", new Pais("Francia"));
-        } else {
-            return m;
-        }
-    }
-    */
 }
